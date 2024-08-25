6b6f0b3a8ce159bb867a453bb36a7f8586fc559f
An Manh <superavm2004@gmail.com>
2024-08-25 18:10:15 +0700

    Add initial project files

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.*;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtX, txtY;
+    TextView txtResult;
+    Button btnPlus;
+    private void innitControl(){
+        txtX = findViewById(R.id.txtX);
+        txtY = findViewById(R.id.txtY);
+        txtResult = findViewById(R.id.txtResult);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x + y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        innitControl();
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+}
